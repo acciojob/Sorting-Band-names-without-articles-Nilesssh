db57a23dd47b9048bfaa57dffec5a9d1ddc8208a
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-04-21 11:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+let ans = [];
+
+touristSpots.forEach(element => {
+	if(element.startsWith("The"||"A"||"An")){
+			ans.push(element);
+	}else{
+		ans.unshift(element);
+	}
+});
+let str = "";
+ans.forEach(element => {
+	str += `<li>${element}</li>`;
+});
+document.getElementById("band").innerHTML = str;
 
